18f32c5c5ba13489583825a3b7ddf15e899b8c5c
Stanislav <yanut995@gmail.com>
2024-05-31 10:30:00 +0300

    Поправил

--- app.js
+++ app.js
@@ -1,36 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-<<<<<<< HEAD
-const fs = require("fs");
-const http = require("http");
-const https = require("https");
-=======
->>>>>>> da6660b (Минимально обновил)
 require("dotenv").config();
 
 const telegraf = require("./routers/telegraf");
 
-<<<<<<< HEAD
-const options = {
-  cert: fs.readFileSync("./ssl/luxeivan.ru_cert.pem"),
-  key: fs.readFileSync("./ssl/luxeivan.ru_private_key.pem"),
-};
-
-const secretSession = process.env.SECRET_SESSION;
-const port = process.env.PORT;
-const portSSL = process.env.PORT_SSL;
-
-const app = express();
-
-app.use("/api/telegraf", telegraf);
-
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(options, app);
-httpServer.listen(port, () => {
-  console.log(`Зашли и вышли, приключения на ${port} порту`);
-});
-httpsServer.listen(portSSL);
-=======
 const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 3000;
@@ -40,11 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 app.use(bodyParser.json());
 app.use("/api/telegraf", telegraf);
 
 app.listen(port, () => {
   console.log(`Зашли и вышли, приключения на ${port} порту`);
 });
->>>>>>> da6660b (Минимально обновил)
