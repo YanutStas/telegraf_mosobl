2fcba634a92dbc0ed883db12478f8d25b399994e
Stanislav <yanut995@gmail.com>
2024-05-31 09:52:32 +0300

    Минимально обновил

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+<<<<<<< HEAD
 const fs = require("fs");
 const http = require("http");
 const https = require("https");
+=======
+>>>>>>> da6660b (Минимально обновил)
 require("dotenv").config();
 
 const telegraf = require("./routers/telegraf");
 
+<<<<<<< HEAD
 const options = {
   cert: fs.readFileSync("./ssl/luxeivan.ru_cert.pem"),
   key: fs.readFileSync("./ssl/luxeivan.ru_private_key.pem"),
@@ -26,3 +30,21 @@ httpServer.listen(port, () => {
   console.log(`Зашли и вышли, приключения на ${port} порту`);
 });
 httpsServer.listen(portSSL);
+=======
+const bodyParser = require("body-parser");
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use(bodyParser.json());
+app.use("/api/telegraf", telegraf);
+
+app.listen(port, () => {
+  console.log(`Зашли и вышли, приключения на ${port} порту`);
+});
+>>>>>>> da6660b (Минимально обновил)
